f2181fc21778143fad9bb3bf2f8bd46e7282e405
grunchin <158331169+grunchin@users.noreply.github.com>
2025-09-19 08:49:10 -0400

    Board configuration

--- Marlin/src/core/boards.h
+++ Marlin/src/core/boards.h
@@ -573,6 +573,7 @@
 
 #define BOARD_RP2040                  6200  // Generic RP2040 Test board
 #define BOARD_BTT_SKR_PICO            6201  // BigTreeTech SKR Pico 1.x
+#define BOARD_PI_PICO                 6202  // Raspberry Pi Pico
 
 //
 // Custom board
